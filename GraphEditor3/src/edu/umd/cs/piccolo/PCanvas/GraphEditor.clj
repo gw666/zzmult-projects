501902f5a4f00c34d6f597d7bf7d318168f5f6ef
Gregg Williams <gregg13@innerpaths.net>
2010-05-07 23:21:45 -0700

    snapshot: includes temp code that tests add-to-node, add-to-edge

--- GraphEditor3/src/edu/umd/cs/piccolo/PCanvas/GraphEditor.clj
+++ GraphEditor3/src/edu/umd/cs/piccolo/PCanvas/GraphEditor.clj
@@ -14,28 +14,27 @@
    ))
 
 (defn -init [width height]
+  ; returns [ signature of superclass (no args), value of state (a map) ]
   [[] { :num-nodes 5, :num-edges 5, :random (Random.) }])
 
 (defn add-to-node [node edge]
-  (let [num-ref (.getAttribute node "num-used")
-        current-count @num-ref]
-    (dosync
-      (aset (.getAttribute node "edges") current-count edge)
-      (alter num-ref inc))))
+  (let [current-count (.getAttribute node "num-used")
+        ]
+    (aset (.getAttribute node "edges") current-count edge)
+    (.addAttribute node "num-used" (inc current-count))))
 
 (defn add-to-edge [edge node]
-  (let [num-ref (.getAttribute edge "num-used")
-        current-count @num-ref]
-    (dosync
-      (aset (.getAttribute edge "nodes") current-count node)
-      (alter num-ref inc))))
+  (let [current-count (.getAttribute edge "num-used")
+        ]
+    (aset (.getAttribute edge "nodes") current-count node)
+    (.addAttribute edge "num-used" (inc current-count))))
 
 (defn -GraphEditorInit [this width height]
   (.setPreferredSize this (Dimension. width height))
   (let [{:keys [num-edges num-nodes random]} (.state this)
         nodeLayer (.getLayer this)
         edgeLayer (PLayer.)
-        node-ref-vector   ; its value is result of loop, on next line
+        node-vector   ; its value is result of loop, on next line
         (loop [result [], x num-nodes]
           (if (zero? x)
             result   ; returned if true
@@ -45,28 +44,38 @@
                                   20
                                   20))
               (dec x))))   ; returned if false
-        n1 (first node-ref-vector)
+        n1 (first node-vector)
         ]
 
-    ; error: no nodes are displayed in window
     (defn install-node [node]
       (println node)
       (.addChild nodeLayer node)
       (.addAttribute node "edges" (make-array PPath num-edges))
-      (.addAttribute node "num-used" (ref 0))
+      (.addAttribute node "num-used" 0)
       )
 
     (.addChild (.getRoot this) edgeLayer)
     (.addLayer (.getCamera this) 0 edgeLayer)
 
-;    (println "vector")
-    (println node-ref-vector)
-;    (doall (map install-node node-ref-vector))
-    (doall (for [nv node-ref-vector]
-      (install-node nv)))
-    (println @(.getAttribute n1 "num-used"))
-    (add-to-node n1 (PPath.))
-    (println @(.getAttribute n1 "num-used"))
+    ;    (println "vector")
+    (println node-vector)
+    ;    (doall (map install-node node-vector))
+    (doall (for [nv node-vector]
+             (install-node nv)))
+
+    ; the test below proves that add-to-node adds new PPaths correctly
+    ; into the array edges, and that num-used can be updated by executing
+    ; addAttribute a second time (no need for refs)
+    (let [p1 (PPath/createEllipse 50 50 20 30)
+          p2 (PPath/createRectangle 100 100 40 20)]
+      (println (.getAttribute n1 "num-used"))
+      (println p1)
+      (println p2)
+      (add-to-node n1 p1)
+      (add-to-node n1 p2)
+      (println (.getAttribute n1 "num-used"))
+      ; should the same as p2
+      (println (aget (.getAttribute n1 "edges") 1)))
     ))
 
 (defn -main []
